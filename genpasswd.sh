c34c5573216bc34a098246712c2fc5baa7632931
Linlin Yan <yanlinlin82@gmail.com>
2016-06-18 12:24:12 +0800

    Improve 'genpasswd'

--- genpasswd.sh
+++ genpasswd.sh
@@ -1,7 +1,13 @@
 genpasswd() {
+	if [ "$1" == "-h" -o "$1" == "--help" ]; then
+		echo "Usage: genpasswd [length [charset]]"
+		echo " e.g.: genpasswd 25"
+		echo "       genpasswd 12 A-Za-z0-9_"
+		return 1
+	fi
 	local LENGTH=$1
 	local CHARSET=$2
 	[ "$LENGTH" == "" ] && LENGTH=20
-	[ "$CHARSET" == "" ] && CHARSET=A-Za-z0-9_
-	tr -dc $CHARSET < /dev/urandom | head -c $LENGTH | xargs
+	[ "$CHARSET" == "" ] && CHARSET='A-Za-z0-9_@#$%^&=+/'
+	cat /dev/urandom | tr -dc "$CHARSET" | head -c $LENGTH | xargs
 }
